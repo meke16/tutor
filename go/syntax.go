fcf233b23a5ecae8261af1a7b8d4458298ca5606
meke16 <habtamucherinet40@gmail.com>
2026-01-03 12:08:56 +0300

    initialize Go module and add main package with hello world example

---
+++ go/syntax.go
@@ -0,0 +1,9 @@
+// Note: In Go, any executable code belongs to the main package.
+package main
+
+import ("fmt")
+
+func main() {
+  fmt.Println("Hello World!")
+  fmt.Println("Welcome to Go programming.")
+}
